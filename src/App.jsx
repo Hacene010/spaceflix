0464089f0bfa560cc38405d3e8a192b080cca091
erriep <tabary.pierre.dev@gmail.com>
2021-05-11 19:14:16 +0200

    fixed conflit and added previous & next buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import MoviesSpace from './components/MoviesSpace/MoviesSpace';
 import MoviesNasa from './components/MoviesNasa/MoviesNasa';
 import MoviesMars from './components/MoviesMars/MoviesMars';
 import TvShow from './components/TvShow/TvShow';
+import './reset.css';
 
 function App() {
   return (
--- src/components/MoviesSpace/MoviesSpace.jsx
+++ src/components/MoviesSpace/MoviesSpace.jsx
@@ -4,21 +4,63 @@ import axios from 'axios';
 
 const SMoviesSpace = styled.div`
   ul {
+    position: relative;
+    flex-wrap: nowrap;
     display: flex;
     list-style-type: none;
     text-align: center;
     margin-top: 5vh;
   }
-  li {
-    position: relative;
+
+  img {
+    width: 100%;
   }
 
-  figcaption {
-    position: absolute;
-    width: 500px;
-    height: 285px;
+  .arrow {
+    width: 20px;
+  }
 
+  .arrowRight {
+    background-color: transparent;
+    border: none;
+    position: absolute;
     bottom: 0;
+    top: 0;
+    right: 10px;
+
+    :hover {
+      background: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0) 0%,
+        rgba(0, 0, 0, 0.65) 100%
+      );
+    }
+  }
+
+  .arrowLeft {
+    background-color: transparent;
+    border: none;
+    position: absolute;
+    bottom: 0;
+    top: 0;
+    left: 10px;
+    z-index: 0;
+    :hover {
+      background: linear-gradient(
+        to left,
+        rgba(0, 0, 0, 0) 0%,
+        rgba(0, 0, 0, 0.65) 100%
+      );
+      z-index: 0;
+    }
+  }
+  figcaption {
+    margin-right: 30px;
+    margin-left: 30px;
+    position: absolute;
+    width: 250px;
+    height: 100%;
+    top: 0;
     background-color: #0008;
     display: flex;
     align-items: center;
@@ -66,6 +108,7 @@ const SMoviesSpace = styled.div`
 
 function MoviesSpace() {
   const [moviesSpace, setMoviesSpace] = useState([]);
+  const [startX, setStartX] = useState(0);
 
   useEffect(() => {
     axios
@@ -75,23 +118,44 @@ function MoviesSpace() {
       .then(({ data }) => {
         setMoviesSpace(data.results);
       });
-  }, []);
+  }, [startX]);
+
+  const slideShowPlus = () => {
+    setStartX(startX + 1);
+  };
+
+  const slideShowMinus = () => {
+    setStartX(startX - 1);
+  };
 
   return (
     <SMoviesSpace>
       <ul>
-        {moviesSpace.map((movie) => {
+        {startX > 0 && (
+          <button className='arrowLeft' onClick={slideShowMinus}>
+            <img className='arrow' src='/img/icon/arrowleft.png' alt='' />
+          </button>
+        )}
+        {moviesSpace.slice(startX, startX + 5).map((movie) => {
           return (
-            <li>
-              {' '}
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                alt=''
-              />
-              <figcaption>{movie.original_title}</figcaption>
-            </li>
+            <>
+              <li>
+                <div className='image'>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                    alt=''
+                  />
+                  <figcaption>{movie.original_title}</figcaption>
+                </div>
+              </li>
+            </>
           );
         })}
+        {startX < 14 && (
+          <button className='arrowRight' onClick={slideShowPlus}>
+            <img className='arrow' src='/img/icon/arrowright.png' alt='' />
+          </button>
+        )}
       </ul>
     </SMoviesSpace>
   );
